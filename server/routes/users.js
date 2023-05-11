738c617b7ce83fe5cccf90ff09f4e79692032050
vedant <ursvedantyetekar@gmail.com>
2023-05-11 16:58:19 +0530

    cleaned files

--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-import { signin, signup } from "../controllers/user.js";
-import express from "express";
-
-const router = express.Router();
-
-router.post("/signin", signin);
-router.post("/signup", signup);
-
-export default router;
